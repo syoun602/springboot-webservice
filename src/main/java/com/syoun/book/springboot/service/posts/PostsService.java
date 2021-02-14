9cba96f42410a93e0fa4ebe22ed030f4459b33bc
SunYong <sunyong02@ajou.ac.kr>
2021-02-14 16:28:33 +0900

    5장. oauth 제외하고 추가하기

--- src/main/java/com/syoun/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/syoun/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.syoun.book.springboot.service.posts;
 
 import com.syoun.book.springboot.domain.posts.Posts;
 import com.syoun.book.springboot.domain.posts.PostsRepository;
+import com.syoun.book.springboot.web.dto.PostsListResponseDto;
 import com.syoun.book.springboot.web.dto.PostsResponseDto;
 import com.syoun.book.springboot.web.dto.PostsSaveRequestDto;
 import com.syoun.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,10 +10,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
@@ -25,6 +36,13 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById(Long id) {
